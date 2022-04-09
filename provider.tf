710c07cfb1be8c6f78fd54be30090992458a0ac3
gokul <gokulramasamy231@gmail.com>
2022-04-09 16:41:04 +0200

    update new1

--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
+line1
+line2
+line3
